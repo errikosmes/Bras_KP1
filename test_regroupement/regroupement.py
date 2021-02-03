fb5c9c1984086fc9b855e46fc85ce8c5d67083fa
Errikos Messara <errikos.messara@gmail.com>
2021-02-03 19:27:52 +0100

    clean up

--- test_regroupement/regroupement.py
+++ test_regroupement/regroupement.py
@@ -1,4 +1,11 @@
-
+"""
+TODO
+- remove prints
+- changer noms des fonctions
+- faire un main + fonctions
+- add API source files
+- def video stream -> main
+"""
 
 # Imports
 from niryo_one_tcp_client import *
@@ -8,6 +15,7 @@ import numpy as np
 from time import sleep
 import time
 
+# Inits
 # Set robot address
 #robot_ip_address = "10.10.10.10"
 robot_ip_address = "169.254.200.200"
@@ -18,8 +26,8 @@ observation_pose = PoseObject(
     roll=0, pitch=1.57, yaw=-0.2,
 )
 
-
-def change_space(px_x,px_y,offset_x=0,offset_y=0):
+# Functions
+def change_space(px_x, px_y, offset_x=0, offset_y=0):
     lg_x = 0.178
     lg_y = 0.188
     size_img = 480
@@ -28,32 +36,33 @@ def change_space(px_x,px_y,offset_x=0,offset_y=0):
     yi = px_y*(lg_y/size_img)
 
     x0 = 0.163
-    y0=-0.093
+    y0 = -0.093
 
-    x=xi+x0+offset_x
-    y=yi+y0+offset_x
+    x = xi+x0+offset_x
+    y = yi+y0+offset_x
 
-    return x,y
+    return x, y
 
 def clean_line(img,lines):
     lines_cpy= np.copy(lines)
 
-    for i in range ( len(lines) ):
-        for j in range ( len(lines) ):
+    for i in range (len(lines)):
+        for j in range (len(lines)):
             if (i!=j) and (lines[i][0][0] / lines[j][0][0] <= 1.13) and  (lines[i][0][0] / lines[j][0][0] >= 0.9) :
-                if ( (lines_cpy[j][0][0]!= 0) and ( lines_cpy[i][0][0]!=0) ):
+                if ((lines_cpy[j][0][0]!= 0) and (lines_cpy[i][0][0]!=0)):
                     lines_cpy[j][0][0]= 0
 
 
-    lines_net=[i for i in lines_cpy if int(i[0][0]) != 0 and (int(i[0][0]) <=len(img[0])-5) ]
+    lines_net = [i for i in lines_cpy if int(i[0][0]) != 0 and (int(i[0][0]) <=len(img[0])-5)]
     print('Clean lines ok')
+    
     return lines_net
 
-
 def find_croisement(lines):
-    horiz =[]
+    horiz = []
     vert = []
-    inter=[]
+    inter = []
+
     for i in lines:
         if i[0][1] < 0.5 and i[0][1] > -0.5:
             horiz.append(i)
@@ -62,27 +71,28 @@ def find_croisement(lines):
         else:
             print('Droite ni horizontale ni verticale',i)
 
-    for i in range ( len(horiz)  ):
-        x0i= np.cos(horiz[i][0][1]) * horiz[i][0][0]
-        y0i= np.sin(horiz[i][0][1]) * horiz[i][0][0]
-        for j in range ( len(vert) ):
-            y0j= np.sin(vert[j][0][1]) * vert[j][0][0]
+    for i in range (len(horiz)):
+        x0i = np.cos(horiz[i][0][1]) * horiz[i][0][0]
+        y0i = np.sin(horiz[i][0][1]) * horiz[i][0][0]
+
+        for j in range (len(vert)):
+            y0j = np.sin(vert[j][0][1]) * vert[j][0][0]
             inter.append((int(x0i), int(y0i+y0j)))
+    
     print('Find croisement ok')
     return(inter)
 
 def line_inter(line_img):
+    gray = cv.cvtColor(line_img, cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(gray, 50, 150, apertureSize = 3)
+    lines = cv.HoughLines(edges, 1, np.pi/180, 300)
+    
+    if lines is None: return None
 
-    gray = cv.cvtColor(line_img,cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray,50,150,apertureSize = 3)
-    lines = cv.HoughLines(edges,1,np.pi/180,300)
-    if lines is None:
-        return None
-
-    lines_net = clean_line(line_img,lines)
-
+    lines_net = clean_line(line_img, lines)
     inter = find_croisement(lines_net)
-    for i in range ( len(lines_net) ):
+
+    for i in range (len(lines_net)):
         for rho,theta in lines_net[i]:
             print('Lines :', i)
             a = np.cos(theta)
@@ -94,7 +104,7 @@ def line_inter(line_img):
             x2 = int(x0 - 1000*(-b))
             y2 = int(y0 - 1000*(a))
                 # and (y0 <= 10):
-            cv2.line(line_img,(x1,y1),(x2,y2),(0,0,255),1)
+            cv2.line(line_img, (x1,y1), (x2,y2), (0,0,255), 1)
 
     return inter
 
@@ -138,7 +148,7 @@ def selectRectCallback(event, x, y, flags, param):
 
 def drawSelected(img, rectCenter, size, nb):
     font      = cv2.FONT_HERSHEY_SIMPLEX
-    offset    =  (20, -40)
+    offset    = (20, -40)
     fontScale = 0.75
     fontColor = (0, 255, 0)
     lineType  = 2
@@ -151,42 +161,33 @@ def drawSelected(img, rectCenter, size, nb):
 
 def video_stream(niryo_one_client):
     # Getting calibration param
-    print("1")
     _, mtx, dist = niryo_one_client.get_calibration_object()
     # Moving to observation pose
-    print("i am here")
     niryo_one_client.move_pose(*observation_pose.to_list())
 
-
-    while "workspace_not_fine" :
-
-        init=time.time()
+    while "workspace_not_found" :
+        init = time.time()
+        
         # Getting image
         status, img_compressed = niryo_one_client.get_img_compressed()
         if status is not True:
             print("error with Niryo One's service")
             break
-        # Uncompressing image
-        img_raw = uncompress_image(img_compressed)
-        # Undistorting
-        img_undistort = undistort_image(img_raw, mtx, dist)
-        # Trying to find markers
-        workspace_found, res_img_markers = debug_markers(img_undistort)
+
+        img_raw = uncompress_image(img_compressed) # Uncompressing image
+        img_undistort = undistort_image(img_raw, mtx, dist) # Undistorting
+        workspace_found, res_img_markers = debug_markers(img_undistort) # Trying to find markers
+
         # Trying to extract workspace if possible
-        if workspace_found:
-            img_workspace = extract_img_workspace(img_undistort, workspace_ratio=1.0)
-        else:
-            img_workspace = None
+        if workspace_found: img_workspace = extract_img_workspace(img_undistort, workspace_ratio=1.0)
+        else: img_workspace = None
 
         # - Display
         # Concatenating raw image and undistorted image
         concat_ims = concat_imgs((img_raw, img_undistort))
 
-
-
         # Concatenating extracted workspace with markers annotation
-        if img_workspace is not None:
-            res_img_markers = concat_imgs((res_img_markers, resize_img(img_workspace, height=res_img_markers.shape[0])))
+        if img_workspace is not None: res_img_markers = concat_imgs((res_img_markers, resize_img(img_workspace, height=res_img_markers.shape[0])))
 
         # Showing images
         show_img("Images raw & undistorted", concat_ims, wait_ms=0)
@@ -198,11 +199,8 @@ def video_stream(niryo_one_client):
         else:
             line_img=None
 
-    
-    x1 =time.time()
-
-    x3 =time.time()
-
+    x1 = time.time()
+    x3 = time.time()
 
     POI = line_inter(line_img) #Points Of Interest
     POISelected = []
@@ -212,22 +210,20 @@ def video_stream(niryo_one_client):
     show_img('Workspace', line_img, wait_ms=10)
     cv2.setMouseCallback('Workspace', selectRectCallback, param=[POI, POISelected, regionSize]) 
     imgCached = line_img.copy()
-    while True :
+    
+    while True:
         # draw region of interest rectangles 
         for point in POI: 
             if point in POISelected: drawSelected(line_img, point, regionSize, POISelected.index(point))
             else: drawUnselected(line_img,point,regionSize)
         
-
         key = show_img('Workspace', line_img)
         line_img = imgCached.copy()
 
         if (key & 0xFF) is (ord('\n') or ord('\r')):  # Will break loop if the user press Escape or Q
             break
-    x3 =time.time()
-
-
-
+    
+    x3 = time.time()
 
     # # PICK FROM X,Y
     # while True:
@@ -254,17 +250,13 @@ def video_stream(niryo_one_client):
 
     niryo_one_client.set_learning_mode(True)
 
-
-
 if __name__ == '__main__':
     # Connect to robot
     client = NiryoOneClient()
     client.connect(robot_ip_address)
     # Calibrate robot if robot needs calibration
-    print("connecte")
     client.calibrate(CalibrateMode.AUTO)
     # Launching main process
-    print("avant")
     video_stream(client)
     # Releasing connection
     client.quit()
