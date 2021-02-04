b5bb9256b2686bfad2f116389673977e3f5f3354
errikosmes <errikos.messara@gmail.com>
2021-02-04 11:54:09 +0100

    clean up

--- interface_qt/UIUItest_qtcreato_pyqt.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '/home/leo/Documents/Bras_KP1/interface_qt/UItest_qtcreato_pyqt.py'
-#
-# Created by: PyQt5 UI code generator 5.10.1
-#
-# WARNING! All changes made in this file will be lost!
-
