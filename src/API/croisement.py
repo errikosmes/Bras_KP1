5ec6fc7dabd9945a6f727f09636e4037c48e7da2
errikosmes <errikos.messara@gmail.com>
2021-02-10 11:32:14 +0100

    Revert "Descritpion et pulti-ligne"
    This reverts commit 96fb4815739994882256050c17c5595ac776a46b.

--- src/API/croisement.py
+++ src/API/croisement.py
@@ -2,234 +2,85 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 29 09:57:22 2021
-
 @author: kat
 """
 
-import cv2
+import cv2 as cv
 import numpy as np
 
 # =============================================================================
 # FONCTION
 # =============================================================================
 
-def dist_eucli(x0,y0,x1,y1):                                                     
-    """
-    Calcul la distance euclidiènne entre deux points repérés avec des coordonnées cartésiennes
+def circle_inter(line_img,inter):
+    cpt=0
+    if inter is None:
+        return 
+    for i in inter:                  
+        line_img = cv.circle(line_img, i , radius=15, color=(0, 255, 255), thickness=2)
+        line_img = cv.putText(line_img, str(cpt) , i, cv.FONT_HERSHEY_SIMPLEX , 2, color=(0,0,0), thickness=3) 
+        cpt+=1
+
+
+def line_inter(line_img):
+                
+    gray = cv.cvtColor(line_img,cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(gray,50,150,apertureSize = 3)
+    lines = cv.HoughLines(edges,1,np.pi/180,200)
+    if lines is None:
+        return None
+    lines_net = clean_line(line_img,lines)
+    inter = find_croisement(lines_net)
+    for i in range ( len(lines_net) ):
+        for rho,theta in lines_net[i]:  
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+                # and (y0 <= 10):
+            cv.line(line_img,(x1,y1),(x2,y2),(0,0,255),1)
     
-    Paramètres
-    ----------
-    x0 : float 
-        La coordonnée du premier point selon x  
-    y0 : float 
-        La coordonnée du premier point selon y 
-    x1 : float 
-        La coordonnée du second point selon x 
-    y1 : float 
-        La coordonnée du second point selon y 
-    
-    Retour
-    ------
-    dist : float
-        la distance euclidiènne entre les deux points
-    """
+    return inter
+
+
+def dist_eucli(x0,y0,x1,y1):
     dx=x1-x0
     dy=y1-y0
     return(np.sqrt(dx**2+dy**2))
     
-def clean_line(img, lines, th_btw_2_lines):                                     
-    """
-    Supprime les lignes redondantes ou trop proche l'une de l'autre  en fonction d'un seuil
-     ainsi que les lignes que l'on detecte au bord de l'image
-    
-    Paramètres
-    ----------
-    img : Matrix of int  n x m
-        n lignes, m colonnes
-        L'image dans laquelle on souhaite supprimer les ligne trouver ( nécéssaire pour enlever les lignes sur les bords )
-    lines : Matrix of float n x 1 x 2 
-        n lignes,
-        Le tableau de ligne à nettoyer
-    th_btw_2_lines : float
-        Le seuil en pourcentage pour savoir si deux lignes peuvent être considéré comme appartenant au même objet
-    
-    Retour
-    ------
-    lines_net : Matrix of float n x 1 x 2
-        Tableau de dimension inférieur ou égale à lines contenant uniquement les lignes "importantes"
-    
-    """
+def clean_line(img, lines): 
     lines_cpy= np.copy(lines)
 
     for i in range ( len(lines) ):
         for j in range ( len(lines) ):
-            if (i!=j) and (lines[i][0][0] / lines[j][0][0] <= 1 + th_btw_2_lines ) and  (lines[i][0][0] / lines[j][0][0] >= 1 - th_btw_2_lines) : 
-                if ( (lines_cpy[j][0][0]!= -1) and (lines_cpy[i][0][0]!=-1) ): 
-                    lines_cpy[j][0][0]= -1
+            if (i!=j) and (lines[i][0][0] / lines[j][0][0] <= 1.1) and  (lines[i][0][0] / lines[j][0][0] >= 0.9) : 
+                if ( (lines_cpy[j][0][0]!= 0) and ( lines_cpy[i][0][0]!=0) ): 
+                    lines_cpy[j][0][0]= 0
 
        
-    lines_net=[i for i in lines_cpy if int(i[0][0]) != -1 and (int(i[0][0]) <=len(img[0])-5) ]
+    lines_net=[i for i in lines_cpy if int(i[0][0]) != 0 and (int(i[0][0]) <=len(img[0])-5) ]
     return lines_net
 
-def angle_dedans(angle, tab_angle, th_same_angle):                                     
-    """
-    Permet de savoir si un angle ( modulo pi ) se trouve dans un tableau d'angle
-      
-    Paramètres
-    ----------
-    angle : float
-        L'angle dont l'on souhaite determiner la position
-    tab_angle : list of float
-        Le tableau contenant plusieurs angles
-    th_same_angle : float 
-        Un seuil ( en radian ) pour definir si deux angles sont à peu près égaux
-    
-    Retour
-    ------
-    ret : bool
-        True si l'élément est présent dans le tableau, False sinon
-    
-    
-    """
-    ret = False
-    for i in tab_angle:
-        if ( angle % np.pi <= (i % np.pi) + th_same_angle ) and ( angle % np.pi >= (i % np.pi) - th_same_angle ) :
-            ret = True
-            return ret
-        
-    return ret
-
-def arg_angle(angle, tab_angle, th_same_angle):
-    """
-    Permet de retrouver la première postion d'un angle dans un tableau d'angle
-    
-    Paramètres
-    ----------
-    angle : float
-        L'angle dont l'on souhaite determiner la position
-    tab_angle : list of float
-        Le tableau contenant plusieurs angles
-    th_same_angle : float
-        La valeur en radian pour déterminer si deux angles sont semblables
-        
-    Retour
-    ------
-    pos : int 
-        La position de l'élément trouvé, retourne -1 si il est absent du tableau
-    
-    """
-    
-    pos = -1
-    
-    for i in range (len(tab_angle)) :
-        if angle <= tab_angle[i] + th_same_angle and  angle >= tab_angle[i] - th_same_angle :
-            pos=i
-            break
-    return(pos)
-
-
-def pt_semblable(pt1,pt2,th_xy_pt):
-    """
-    Permet de determiner si deux points peuvent être considérer comme identique
-    
-    Paramètres
-    ----------
-    
-    pt1 : tuple of float 
-        Les coordonnées cartésiennes du premier point sous forme d'un tuple ( dans un repère cartesien )
-    pt2 : tuple of float 
-        Les coordonnées cartésiennes du second point également sous la forme d'un tuple ( dans un repère cartesien )
-    th_xy_pt : float 
-        Un seuil sur x et y permettant de juger de la proximité entre les deux points
-    
-    Retour
-    ------
-    ret : Bool
-        True si les points sont semblable, False sinon 
-    
-    """
-    ret = False
-    if (pt1[0] >= pt2[0] - th_xy_pt and pt1[0] <= pt2[0] + th_xy_pt) and ( pt1[1] >= pt2[1] - th_xy_pt and pt1[1] <= pt2[1] + th_xy_pt) : 
-        ret = True
-        return ret
-    return ret
-
-def clean_points(tab_pts, th_xy_pt):
-    """
-    Supprime les points trop proches entre eux en fonction d'un seuil
-    
-    Paramètres
-    ----------
-    tab_pts :  Matrix of int n x 2 ( codé sur 64 bits )
-        n lignes,
-        Le tableau de point à nettoyer
-    th_xy_pt : float 
-        Un seuil sur x et y permettant de juger de la proximité entre les deux points
-    
-    Retour
-    ------
-    pts_net : Matrix of int n x 2 ( codé sur 64 bits )
-        Tableau de dimension inférieur ou égale à tab_pts contenant uniquement les points non redondant
-    
-    """
-    pt_cpy= np.copy(tab_pts)
-    pts_net = []
-    for i in range ( len(tab_pts) ):
-        for j in range ( i+1, len(tab_pts) ) :
-
-            if pt_semblable(tab_pts[i],tab_pts[j],th_xy_pt) or tab_pts[j][0] < 0 or tab_pts[j][1] < 0:
-                pt_cpy[j][0] = -1
-                pt_cpy[j][1] = -1
-
-    pts_net=[i for i in pt_cpy if i[0] != -1 and i[1] != -1 ]
-    return(pts_net)
-    
 
 def type_croisement(lines):
-    """
-    Permet de determiner le nombre de type de droites, c'est à dire le nombre de droites avec des coefficients directeur différents. Des droites
-    appartenant au même type sont toutes parralèlle entre elles. 
-    Permet aussi de trier les droites dans une liste selon leurs coefficients directeurs 
-    
-    Paramètres
-    ----------
-    lines :  Matrix n x 1 x 2 
-        n lignes,
-        Le tableau de ligne dont on souhaite determine le type et que l'on souhaite trier
-    
-    Retour
-    ------
-    type_droite : Int 
-        Sa valeur est determiné en fonction du type de droite ( expliqué précédement ) présent dans lines\n
-        type_droite = -1 --> Un ou aucun type de droites \n
-        type_droite = 0 --> 2 types de droites horizontales et verticales\n
-        type_droite = 1 --> 2 types de droites non horizontales et non verticales\n
-        type_droite = 2 --> Plus de deux types de droites
-    droites : List n x m 
-        n types de droites différentes
-        m est le nombre de droites d'un certain type de droite
-        droites est donc un tableau de droites trier en fonction de la pente des droites
-        
-    """
-    
     first = lines[0][0][1]
-
     droites = []
     droites.append(first)
     for i in lines:
-        if  not angle_dedans(i[0][1],droites, 0.1):
+        if  not angle_dedans(i[0][1],droites):
             droites.append(i[0][1])
             #une erreur peut se glisser au dessus
     if len(droites) != 2:
         type_droite = 2
         print("Plus de deux types droites")
 
-    elif len(droites) == 0 or len(droites) == 1 :
-        type_droite = -1
-        print('Une ou aucune droite')
     
     else:
-        if ( droites[0] % (np.pi/2) >= - 0.1 ) and (droites[1] % (np.pi/2) >= - 0.1) and ( droites[0] % (np.pi/2) <= 0.1 ) and (droites[1] % (np.pi/2) <=  0.1):
+        if ( droites[0] % (np.pi/2) >= -0.1 ) and (droites[1] % (np.pi/2) >= -0.1) and ( droites[0] % (np.pi/2) <= 0.1 ) and (droites[1] % (np.pi/2) <= 0.1):
             type_droite = 0
             print('Droite horizontale et verticale')
             
@@ -239,120 +90,83 @@ def type_croisement(lines):
     
     return(type_droite,droites)
 
+# type_droite = 0 -> droite horizontale, verticale
+# type_droite = 1 -> droite non horizontale, non verticale
+# type_droite = 2 -> Plus de deux types de droite
+
+def angle_dedans(angle,tab):
+    for i in tab:
+        if ( angle % np.pi <= (i % np.pi) + 0.1 ) and ( angle % np.pi >= (i % np.pi) - 0.1 ) :
+            return True
+        
+    return False
+
+
 def find_croisement(lines):
-    """
-    Trouve tout les croisements dans un ensemble de droite, 
-    
-    Paramètres
-    ----------
-    lines :  Matrix of floats n x 1 x 2 ( codé sur 64 bits )
-        n lignes,
-        Le tableau de ligne dont on souhaite trouver les intersections.       
-    
-    Retour
-    ------
-    inter : list of int tuples 
-        Tableau des points d'intersection des droites contenu dans lines    
-    """
-    
-    
+    horiz =[]
+    vert = []
     inter=[]
     
     type_droite, droites = type_croisement(lines) 
-
     
 # =============================================================================
-#    Sépare en deux types de droite
+#    Sépare en deu types de droite
 # =============================================================================
 
-    tab_of_types = [[] for i in range (len( droites ))]
+    if type_droite != 2:
+        for i in lines:
+            if i[0][1] < droites[1] + 0.1 and i[0][1] > droites[1]-0.1:
+                vert.append(i)
+            elif i[0][1] < droites[0] + 0.1 and i[0][1]  > droites[0] - 0.1 :
+                horiz.append(i)
+            else:
+                print("erreur inconnu")
                 
-    for i in range ( len(lines) ):
-        pos = arg_angle(lines[i][0][1], droites, 0.1)
-        tab_of_types[pos].append(lines[i][0])
-        
-        
+            
 # =============================================================================
 #     Trouve les croisements 
 # =============================================================================
             
-    if type_droite==0 : 
-        for i in range ( len(tab_of_types[0])  ):
-            x0i= np.cos(tab_of_types[0][i][1]) * tab_of_types[0][i][0]
-            y0i= np.sin(tab_of_types[0][i][1]) * tab_of_types[0][i][0]
-            for j in range ( len(tab_of_types[1]) ):
-                y0j= np.sin(tab_of_types[1][j][1]) * tab_of_types[1][j][0]
+            
+    if type_droite==0 :       
+        for i in range ( len(horiz)  ):
+            x0i= np.cos(horiz[i][0][1]) * horiz[i][0][0] 
+            y0i= np.sin(horiz[i][0][1]) * horiz[i][0][0]
+            for j in range ( len(vert) ):
+                y0j= np.sin(vert[j][0][1]) * vert[j][0][0]
                 inter.append(( round(x0i), round(y0i+y0j) ))
-
     
-    elif (type_droite > 0)  :
-        for i in range ( len(tab_of_types)  ):                                 
-            for j in range ( len(tab_of_types[i]) ):                            
-                
-               x0i= np.cos(tab_of_types[i][j][1]) * tab_of_types[i][j][0] 
-               y0i= np.sin(tab_of_types[i][j][1]) * tab_of_types[i][j][0]
-               x1i = int(x0i - 100* np.sin(tab_of_types[i][j][1]) )
-               y1i = int(y0i + 100* np.cos(tab_of_types[i][j][1]) )
-               
-               coef_dir_i = (y1i-y0i)/(x1i-x0i)
-               ord_ori_i = y0i - coef_dir_i * x0i
-               
-               for k in range (i+1, len(tab_of_types) ):
-                   for l in range ( len(tab_of_types[k]) ):      
-                       x0j= np.cos(tab_of_types[k][l][1]) * tab_of_types[k][l][0] 
-                       y0j= np.sin(tab_of_types[k][l][1]) * tab_of_types[k][l][0]
-                       
-                       x1j = int(x0j + 100 * np.sin(tab_of_types[k][l][1]) )
-                       y1j = int(y0j - 100 * np.cos(tab_of_types[k][l][1]) )
-                       
-                       coef_dir_j = (y1j-y0j)/(x1j-x0j)
-                       ord_ori_j = y0j - coef_dir_j * x0j
-                       
-                       inter_x = (ord_ori_j - ord_ori_i ) /  ( coef_dir_i - coef_dir_j)
-                       inter_y = coef_dir_i * inter_x + ord_ori_i
-
-                       inter.append(   (round(inter_x), round(inter_y))   )
-    else:
-        print('erreur')
     
+
+    if type_droite == 1 : 
+        for i in range ( len(vert)  ):
+           x0i= np.cos(vert[i][0][1]) * vert[i][0][0] 
+           y0i= np.sin(vert[i][0][1]) * vert[i][0][0]
+           x1i = int(x0i - 100* np.sin(vert[i][0][1]) )
+           y1i = int(y0i + 100* np.cos(vert[i][0][1]) )
+           
+           coef_dir_i = (y1i-y0i)/(x1i-x0i)
+           ord_ori_i = y0i - coef_dir_i * x0i
+
+               
+           for j in range ( len(horiz) ):
+               x0j= np.cos(horiz[j][0][1]) * horiz[j][0][0]
+               y0j= np.sin(horiz[j][0][1]) * horiz[j][0][0]
+               x1j = int(x0j + 100 * np.sin(horiz[j][0][1]) )
+               y1j = int(y0j - 100 * np.cos(horiz[j][0][1]) )
+               
+               coef_dir_j = (y1j-y0j)/(x1j-x0j)
+               ord_ori_j = y0j - coef_dir_j * x0j
+               
+               inter_x = (ord_ori_j - ord_ori_i ) /  ( coef_dir_i - coef_dir_j)
+               inter_y = coef_dir_i * inter_x + ord_ori_i
+               
+
+               inter.append(   (round(inter_x), round(inter_y))   )
+               # inter.append(   (round(x1j), round(y1j))   )
+               
+    print('\ninter',inter)
+    print('droites',droites)
+    print('\nLEN x',len(horiz))
+    print('LEN y',len(vert))
     return(inter)
-        # 
-
-img = cv2.imread('robot_image2.JPG')
-
-# img = cv2.imread('img2.jpg')
-
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
-lines = cv2.HoughLines(edges,1,np.pi/180,300)
-
-lines_net = clean_line(img,lines, 0.1)
-inter = find_croisement(lines_net)
-inter_net = clean_points(inter,0.)
-        
-        
-        
-# =============================================================================
-# AFFICHAGE
-# =============================================================================
-
-cpt=0
-for i in range ( len(lines_net) ):
-    for rho,theta in lines_net[i]:  
-        
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),1)
-for i in inter_net:
-    img = cv2.circle(img, tuple(i) , radius=15, color=(0, 255, 255), thickness=2)
-    img = cv2.putText(img, str(cpt) , tuple(i), cv2.FONT_HERSHEY_SIMPLEX , 2, color=(0,0,0), thickness=3) 
-    cpt += 1
-
-cv2.imwrite('img_trans24444.jpg',img)
-
