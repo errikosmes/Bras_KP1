8f589706fbe58cc1d184b14f67e1d51a02c3cdeb
errikosmes <errikos-panagiotis.messara@etu.univ-grenoble-alpes.fr>
2021-03-10 10:31:55 +0100

    bug fix: "enter" when nb_inter is not nb_obj

--- src/main.py
+++ src/main.py
@@ -145,7 +145,7 @@ def main_thread(client):
             key = show_img('Workspace', line_img)
             line_img = imgCached.copy()
 
-            if key in [27, ord('\n'), ord('\r'), ord("q")]:  # Will break loop if the user press Escape or Q
+            if ((key in [27, ord('\n'), ord('\r'), ord("q")]) and (len(tab_pose) is len(POISelected))):  # Will break loop if the user press Escape or Q
                 break
             lock.lockForRead()
             not_quit_n_not_exec = not(kill) and not(execute)
